7bb4675cd3064e0a691b7e2721bfee07680d414d
Hunter Hu <hunterhu.ca@gmail.com>
2023-08-15 17:57:53 -0400

    Unpacking dictionary

--- unpacking.py
+++ unpacking.py
@@ -11,3 +11,9 @@ print(*list(range(5)))
 first = [1,2,3]
 second= [4,5,6]
 print([*first, *second, *"hello"])
+
+#unpacking dictionary
+first = {"x":1}
+second= {"x":10, "y":20}
+combined={**first, **second, "z":30}
+print(combined)
